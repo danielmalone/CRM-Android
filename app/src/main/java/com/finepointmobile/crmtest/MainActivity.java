ca2a772da0a0cc220df68843f63bd6e82479da78
Daniel Malone <danieljmalone@gmail.com>
2017-06-13 21:26:28 -0500

    make RecyclerView work

--- app/src/main/java/com/finepointmobile/crmtest/MainActivity.java
+++ app/src/main/java/com/finepointmobile/crmtest/MainActivity.java
@@ -1,15 +1,22 @@
 package com.finepointmobile.crmtest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    ArrayList<User> mUsers;
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +25,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        mUsers = new ArrayList<>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 0; i < 100; i++) {
+            mUsers.add(new User("Daniel", "Malone", "daniel@finepointmobile.com"));
         }
 
-        return super.onOptionsItemSelected(item);
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddUser.class));
+            }
+        });
+
+        loadRecyclerView();
+    }
+
+    private void loadRecyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new MainAdapter(mUsers);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
